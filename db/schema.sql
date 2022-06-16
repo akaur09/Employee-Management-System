99e489b8c99cb6cda5cda08982775cdb832bf59a
akaur091 <amniderkaur.09@gmail.com>
2022-06-15 18:24:44 -0700

    add department table

--- db/schema.sql
+++ db/schema.sql
@@ -2,3 +2,10 @@ DROP DATABASE IF EXISTS business;
 CREATE DATABASE business;
 
 USE business;
+
+-- add tables
+CREATE TABLE departments (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VACHAR(30),
+    PRIMARY KEY(id)
+);
