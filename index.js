738f624e70a2688a09e6e22eda85b69bf2d8cd0e
akaur091 <amniderkaur.09@gmail.com>
2022-06-15 21:27:04 -0700

    add questions to update an employee:

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const inquirer = require('inquirer');
 // require sql
 const mysql = require("mysql");
 
-const questions= [
+const questions = [
     "VIEW ALL DEPARTMENTS",
     "VIEW ALL ROLES",
     "VIEW ALL EMPLOYEES",
@@ -16,7 +16,7 @@ const questions= [
 ];
 
 // empty array
-var list = []; 
+var list = [];
 
 // create a sql connection
 const db = mysql.createConnection(
@@ -31,7 +31,7 @@ const db = mysql.createConnection(
 
 // create a funciton to display the menu and make choices
 function menu() {
-    inquirer.prompt ([
+    inquirer.prompt([
         {
             name: "option",
             type: "list",
@@ -39,28 +39,28 @@ function menu() {
             choices: questions,
         },
     ])
-    .then ((data) => {
-        if (data.option == questions[0]){
-            viewDepartment();
-        } else if (data.option == questions[1]){
-            viewRoles();
-        } else if (data.option == questions[2]){
-            viewEmployees();
-        } else if (data.option == questions[3]){
-            addDepartment();
-        } else if (data.option == questions[4]){
-            addRole();
-        } else if (data.option == questions[5]){
-            addEmployee();
-        } else if (data.option == questions [6]){
-            updateEmployee();
-        } else if (data.option == questions[7]){
-            byeBye();
-        } else {
-            console.log(data);
-            console.log (questions[0]);
-        }
-    });
+        .then((data) => {
+            if (data.option == questions[0]) {
+                viewDepartment();
+            } else if (data.option == questions[1]) {
+                viewRoles();
+            } else if (data.option == questions[2]) {
+                viewEmployees();
+            } else if (data.option == questions[3]) {
+                addDepartment();
+            } else if (data.option == questions[4]) {
+                addRole();
+            } else if (data.option == questions[5]) {
+                addEmployee();
+            } else if (data.option == questions[6]) {
+                updateEmployee();
+            } else if (data.option == questions[7]) {
+                byeBye();
+            } else {
+                console.log(data);
+                console.log(questions[0]);
+            }
+        });
 }
 
 // create function to select which department to view
@@ -106,7 +106,7 @@ function viewEmployees() {
     });
 }
 // create a function to add a department
-function addDepartment(){
+function addDepartment() {
     // use inquirer prompt to ask if user wants to add department
     inquirer.prompt([
         {
@@ -115,18 +115,18 @@ function addDepartment(){
             type: "input",
         },
     ])
-    .then((answer) => {
-        let sql =`INSERT INTO departments (name)
+        .then((answer) => {
+            let sql = `INSERT INTO departments (name)
         VALUES ('${answer.departName}');`;
-        db.query(sql, (error, result) => {
-            if (error) throw error;
-            console.log (`\n DEPARTMENT ${answer.departName} made successfully \n`);
-            menu();
+            db.query(sql, (error, result) => {
+                if (error) throw error;
+                console.log(`\n DEPARTMENT ${answer.departName} made successfully \n`);
+                menu();
+            });
         });
-    });
 }
 // create a function that adds a new role into roles table
-function addRole(){
+function addRole() {
     // select a department
     let allDepartments = [];
     let sqlOne = `SELECT * FROM departments`;
@@ -158,27 +158,27 @@ function addRole(){
             choices: allDepartments,
         },
     ])
-    .then((answer) => {
-        let depart_id = 0;
-        for ( let x = 0; x < allDepartments.length; x++){
-            if (answer.reportsTo == allDepartments[x]) {
-                depart_id = x + 1;
+        .then((answer) => {
+            let depart_id = 0;
+            for (let x = 0; x < allDepartments.length; x++) {
+                if (answer.reportsTo == allDepartments[x]) {
+                    depart_id = x + 1;
+                }
             }
-        }
-        // add function to insert into table
-        let sql = `
+            // add function to insert into table
+            let sql = `
         INSERT INTO roles (title, salary, department_id)
         VALUES ('${answer.rolName}', '${answer.rolSalary}', ${depart_id});
         `;
-        db.query(sql, (error, result) => {
-            if (error) throw error;
-            console.log (`\n ROLE ${answer.rolName} created with a salary of ${answer.rolSalary} \n`);
-            menu();
+            db.query(sql, (error, result) => {
+                if (error) throw error;
+                console.log(`\n ROLE ${answer.rolName} created with a salary of ${answer.rolSalary} \n`);
+                menu();
+            });
         });
-    });
 }
 //  create function to add an employee 
-function addEmployee(){
+function addEmployee() {
     // call on all roles helper function
     let allRoles = getRoles();
     // call on helper function to get all employees
@@ -188,47 +188,78 @@ function addEmployee(){
         {
             name: "firstName",
             message: "Please enter employees first name: ",
-            type: "input", 
+            type: "input",
         },
         {
             name: "lastName",
             message: "Please enter employees last name: ",
-            type: "input", 
+            type: "input",
         },
         {
             name: "assignRole",
             message: "Please select the employees role: ",
             type: "list",
-            choices: allRoles, 
+            choices: allRoles,
         },
         {
             name: "reports",
             message: "Please choose who this employees works under: ",
             type: "list",
-            choices: allEmployees, 
+            choices: allEmployees,
         },
     ])
-    .then ((answer) => {
-        let role_id = 0; 
-        for (let x = 0; x < allRoles.length; q++){
-            if (answer.assignRole == allRoles[x]) {
-                role_id = x + 1;
+        .then((answer) => {
+            let role_id = 0;
+            for (let x = 0; x < allRoles.length; q++) {
+                if (answer.assignRole == allRoles[x]) {
+                    role_id = x + 1;
+                }
             }
-        }
-        let leader_id = 0;
-        for(let x = 0; q < allRoles.length; q++){
-            if (answer.reports == allRoles[q]){
-                leader_id = q + 1;
+            let leader_id = 0;
+            for (let x = 0; q < allRoles.length; q++) {
+                if (answer.reports == allRoles[q]) {
+                    leader_id = q + 1;
+                }
             }
-        }
 
-        let sql = `
+            let sql = `
         INSERT INTO employees (first_name, last_name, role_id, manager_id)
         VALUES ('${answer.first}', '${answer.last}', ${role_id}, ${leader_id});`;
-        db.query(sql, (error, result) => {
-            if (error) throw error;
-            console.log(`\n employee ${answer.first} ${answer.last} added \n`);
-            menu();
+            db.query(sql, (error, result) => {
+                if (error) throw error;
+                console.log(`\n employee ${answer.first} ${answer.last} added \n`);
+                menu();
+            });
         });
-    });
 }
+
+// create a function that updates employee information
+function updateEmployeeRole() {
+    //  call employee helper function
+    let allEmployees = getEmployees();
+    // call roles helper function
+    let allRoles = getRoles();
+    // user inquier to prompt user to update employee
+    inquirer.prompt([
+        {
+            name: "confirm",
+            message: "Are you sure you want to change an employee's role?",
+            type: "confirm",
+            choices: ["press enter"],
+        },
+        {
+            name: "whichEmployee",
+            message: "Please select the employee you wish to update?",
+            type: "list",
+            choices: allEmployees,
+        },
+        {
+            name: "whichRole",
+            message: "which role would you like to assign this employee: ",
+            type: "list",
+            choices: allRoles,
+        },
+    ])
+    
+}
+
