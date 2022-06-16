1ee7c4e854c0c1521fe0d7a59af1c31258fadb24
akaur091 <amniderkaur.09@gmail.com>
2022-06-15 18:34:21 -0700

    add vaules into roles

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,15 @@ VALUES
     ("Sales"),
     ("Human Resources"),
     ("Management");
+
+-- add to roles
+INSERT INTO roles (title, salary, department_id)
+VALUES
+    ("Tax Accountant", 90000, 1),
+    ("Call agent", 75000, 3),
+    ("Senior SalesPerson", ),
+    ("Recruiter", 120000, 2) ,
+    ("Financial Analyst", 135000, 4 ),
+    ("Regional Manager", 130000, 3),
+    ("Human Resource representative", 100000, 4 ),
+    ("Junior SalesPerson", 80000, 1 );
