831ae1acc22c3fe828d216b29b2d295187aac039
akaur091 <amniderkaur.09@gmail.com>
2022-06-15 19:00:15 -0700

    add questions

--- index.js
+++ index.js
@@ -2,3 +2,14 @@
 const inquirer = require('inquirer');
 // require sql
 const mysql = require("mysql");
+
+const questions= [
+    "VIEW ALL DEPARTMENTS",
+    "VIEW ALL ROLES",
+    "VIEW ALL EMPLOYEES",
+    "ADD DEPARTMENT",
+    "ADD ROLE",
+    "ADD EMPLOYEE",
+    "UPDATE ENPLOYEE",
+    "DONE",
+]
